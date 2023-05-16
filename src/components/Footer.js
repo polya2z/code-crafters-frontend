ab825a3a5473b1755fbf79a063ecef02ac6787b0
polya2z <polya2zofficial@gmail.com>
2023-05-16 16:29:51 +0530

    developer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,12 @@ function Footer() {
                         <img src='logo192.png' className='rounded-full w-24'/> 
                         <span class="ml-3 text-xl">Code Crafters</span>
                     </a>
-                    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Code Crafters —
+                    <p class="text-md text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Code Crafters —
                         <a href="https://polya2z.github.io" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@polyA2z</a>
                     </p>
-
+                    <p class="text-md font-semibold text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Developed by 👾
+                        <a href="https://zbytes2227.github.io/" class="text-purple-700 ml-1" rel="noopener noreferrer" target="_blank">@Zbytes</a>
+                    </p>
                 </div>
             </footer>
         </>
